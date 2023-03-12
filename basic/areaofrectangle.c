5241c66e6e0ea710df8078e1b0fd7392de5f7ad8
Aditi4072004 <127680225+Aditi4072004@users.noreply.github.com>
2023-03-12 22:02:58 +0530

    Add files via upload

---
+++ basic/areaofrectangle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=10,b=5;
+printf("area of a rectangle : %d",a*b);
+
+}
---
+++ basic/fertocel.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float fer,cel;
+printf("enter the degree in ferhenhite");
+scanf("%f",&fer);
+cel=((float)5/(float)9)*(fer-32);
+printf("%f",cel);
+return 0;
+}
